a6a53c7bf125b88d8ae7b161151a85ae8b8dafc5
Oleksandr Yanyshyn <yanyshynnastya3@gmail.com>
2024-03-09 15:59:08 +0200

    Added navigation to Home page, added a button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,47 @@
 import React from "react";
+import Button from '@mui/material/Button';
 
 const Navbar = () => {
+
+  const navbarStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: '10px', 
+  };
+
+  const spacerStyle = {
+    flexGrow: 1, 
+  };
+
+
+   const buttonStyle = {
+    position: 'relative',
+    //top: 10,
+    //right: 30,
+    backgroundColor: '#4CAF50', 
+    color: '#FFF',
+    padding: '11px 30px', 
+    textDecoration: 'none',
+    //transition: 'filter 0.3s ease', 
+    //'&:hover': {
+      //filter: 'brightness(90%)',
+    //},
+  };
+
   return (
-    <header>
-      <nav>
-      </nav>
-    </header>
+    
+    <nav style={navbarStyle}>
+      <div>
+        <h1>TimeSquad</h1>
+      </div>
+      <div style={spacerStyle}></div>
+      <div>
+        <Button variant="contained" style={buttonStyle}>
+          Log In
+        </Button>
+      </div>
+    </nav>
   );
 };
 
