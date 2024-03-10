0ed91edb176230caba993b12da5301a38430b855
Oleksandr Yanyshyn <yanyshynnastya3@gmail.com>
2024-03-10 21:59:50 +0200

    Added text, picture and sign up button

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,12 +1,34 @@
 import React from "react";
+import Button from '@mui/material/Button';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+//import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 
 const Main = () => {
+  const buttonStyle = {
+    backgroundColor: '#4CAF50', 
+    borderRadius: '8px', 
+    color: '#ffffff', 
+    padding: '10px 20px', 
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: '10px',
+  };
+
   return (
-    <main>
-      <article className="block">Блок 1</article>
-      <article className="block">Блок 2</article>
-      <article className="block">Блок 3</article>
-    </main>
+    <div className="home-container">
+      <div className="problem-text-container">
+      <p className="ubuntu-condensed-regular">
+          Ласкаво просимо на Time Fighters - твоє особисте просторово-часове поле пригод! Стань володарем свого часу та розвивай свого унікального персонажа, подорожуй між епохами і завданнями, та вигравай битви з часом!
+        </p>
+        <Button variant="contained" style={buttonStyle} endIcon={<ArrowForwardIcon />}>
+          Start!
+        </Button>
+      </div>
+      <div className="pers-image-container">
+        <img src="./images/piligrim.jpg" alt="Your character!" />
+      </div>
+    </div>
   );
 };
 
