55f8ddfa6b11b919a08eca28f4b0847e748cb30b
viktoria <kmrnkviktoria@gmail.com>
2024-03-12 21:44:14 +0200

    added howitworks and possibilities to the main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import Home from "./components/Home";
-import Possibilities from "./components/Possibilities";
-import Howitworks from "./components/Howitworks";
 import './App.css'
 
 function App() {
@@ -10,8 +8,6 @@ function App() {
      <p>Hello project</p> */}
 
      <Home />
-     <Possibilities />
-     <Howitworks />
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import Button from '@mui/material/Button';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
-//import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
+import ScheduleIcon from '@mui/icons-material/Schedule';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
+import StoreIcon from '@mui/icons-material/Store';
+import AssignmentIcon from '@mui/icons-material/Assignment';
+import './font.css';
 
 const Main = () => {
   const buttonStyle = {
@@ -15,21 +20,82 @@ const Main = () => {
     marginTop: '10px',
   };
 
+  const howitworksInfoData = [
+    {
+      title: "створюй завдання, занотовуй плани",
+      icon: <ScheduleIcon />
+    },
+    {
+      title: "з легкістю та задоволенням виконуй їх у вигляді захоплюючої гри",
+      icon: <SportsEsportsIcon />
+    },
+    {
+      title: "долай прокрастинацію!",
+      icon: <HowToRegIcon />
+    },
+  ];
+
+  const PossibilitiesInfoData = [
+    {
+      title: "магазин",
+      icon: <StoreIcon />,
+    },
+    {
+      title: "завдання",
+      icon: <AssignmentIcon />,
+    },
+  ];
+
   return ( 
-  <div className="home-container">
-  <div className="problem-text-container">
-  <p className="ubuntu-condensed-regular">
-      Ласкаво просимо на Time Fighters - твоє особисте просторово-часове поле пригод! Стань володарем свого часу та розвивай свого унікального персонажа, подорожуй між епохами і завданнями, та вигравай битви з часом!
-    </p>
-    <Button variant="contained" style={buttonStyle} endIcon={<ArrowForwardIcon />}>
-      Start!
-    </Button>
-  </div>
-  <div className="pers-image-container">
-    <img src="./images/piligrim.jpg" alt="Your character!" />
-  </div>
-</div>
-);
+    <div className="home-container">
+      <div className="problem-text-container">
+        <p className="ubuntu-condensed-regular">
+          Ласкаво просимо на Time Fighters - твоє особисте просторово-часове поле пригод! Стань володарем свого часу та розвивай свого унікального персонажа, подорожуй між епохами і завданнями, та вигравай битви з часом!
+        </p>
+        <Button variant="contained" style={buttonStyle} endIcon={<ArrowForwardIcon />}>
+          Start!
+        </Button>
+      </div>
+      <div className="howitworks-section-wrapper">
+        <div className="howitworks-section-top">
+          <h1 className="ubuntu-condensed-heading">Як це працює?</h1>
+          <p className="ubuntu-condensed-regular larger-font">
+            Все дуже просто! Грай, збирай нагороди і водночас виконуй поставлені цілі.
+          </p>
+        </div>
+        <div className="howitworks-section-bottom">
+          {howitworksInfoData.map((data, index) => (
+            <div className="howitworks-section-info" key={index}>
+              <h2 className="ubuntu-condensed-regular">{data.title}</h2>
+              {data.icon}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="Possibilities-section-wrapper">
+        <div className="Possibilities-section-top">
+          <h1 className="ubuntu-condensed-heading">різноманітність можливостей</h1>
+        </div>
+        <div className="Possibilities-section-bottom">
+          {PossibilitiesInfoData.map((data, index) => (
+            <div className="Possibilities-section-info" key={index}>
+              <div className="icon-wrapper">
+                {data.icon}
+              </div>
+              <h2 className="ubuntu-condensed-regular">{data.title}</h2>
+              {data.title === "завдання" && (
+                <div className="additional-buttons">
+                  <button className="additional-button">1...</button>
+                  <button className="additional-button">2...</button>
+                  <button className="additional-button">3...</button>
+                </div>
+              )}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
